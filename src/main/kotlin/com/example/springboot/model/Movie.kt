6fa3edd092b55e1f37079e6c4d55da6eca32f70a
gitkaran <karan.thakur@n26.com>
2020-01-20 01:26:08 +0100

    [PR-10] - Add constraints

--- src/main/kotlin/com/example/springboot/model/Movie.kt
+++ src/main/kotlin/com/example/springboot/model/Movie.kt
@@ -1,16 +1,18 @@
 package com.example.springboot.model
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 
 data class Movie(
-        val movieId: Int,
-        val name: String,
-        val description: String
+    @get: NotNull val movieId: Int,
+    @get: NotBlank val name: String,
+    @get: NotBlank val description: String
 )
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class MovieSummary(
-        val id: Int,
-        val title: String?,
-        val overview: String?
+    val id: Int,
+    val title: String?,
+    val overview: String?
 )
