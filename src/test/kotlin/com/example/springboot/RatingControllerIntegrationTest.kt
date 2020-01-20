b7a1e556af2c844fc4dd74cc536fa80d9240fc90
gitkaran <karan.thakur@n26.com>
2020-01-20 01:30:22 +0100

    [PR-11] - Rating Controller Integration Test

--- src/test/kotlin/com/example/springboot/RatingControllerIntegrationTest.kt
+++ src/test/kotlin/com/example/springboot/RatingControllerIntegrationTest.kt
@@ -45,4 +45,16 @@ class RatingControllerIntegrationTest {
                 .content("movieId", Matchers.comparesEqualTo(movieId))
                 .content("rating", Matchers.comparesEqualTo(13))
     }
+
+    @Test
+    fun `Should return 404, when movieId not found`() {
+        val movieId = 1
+        RestAssured.given().log().all()
+                .contentType("application/json")
+                .get("http://localhost:9095/ratings/movies/$movieId")
+                .then().log().body()
+                .statusCode(404)
+                .content("title", Matchers.comparesEqualTo("Movie not available"))
+                .content("message", Matchers.comparesEqualTo("Movie not available"))
+    }
 }
